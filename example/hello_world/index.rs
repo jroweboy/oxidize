5f89bcc171e8c3911c082914419de954388df356
robhobbes <robhobbes@gmail.com>
2014-01-27 10:19:37 -0700

    Changing to two spaces instead of tabs.

--- example/hello_world/index.rs
+++ example/hello_world/index.rs
@@ -18,44 +18,44 @@ use http::status::Status;
 /////////////////////////
 
 //enum Method {
-//	GET,
-//	POST,
-//	WS
+//  GET,
+//  POST,
+//  WS
 //}
 
 
 
 pub struct User {
-	id : uint,
-	is_anonymous : bool,
-	is_authenticated : bool,
-	is_superuser : bool,
+  id : uint,
+  is_anonymous : bool,
+  is_authenticated : bool,
+  is_superuser : bool,
 }
 
 pub struct Request {
-	method : ~str, //Method,
-    GET : HashMap<~str, ~str>,
-    POST : HashMap<~str, ~str>,
+  method : ~str, //Method,
+  GET : HashMap<~str, ~str>,
+  POST : HashMap<~str, ~str>,
 }
 
 pub struct Response {
-	content : ~str,
-	status: Status
-	// add more stuff? I dunno
-	//reason_phrase : ~str,
-	//streaming : bool,
+  content : ~str,
+  status: Status
+  // add more stuff? I dunno
+  //reason_phrase : ~str,
+  //streaming : bool,
 }
 
 impl Response {
-	fn new(status : Status, content : ~str) -> Response {
-		Response { content: content, status: status}
-	}
+  fn new(status : Status, content : ~str) -> Response {
+    Response { content: content, status: status}
+  }
 }
 
 // todo: Make the context vars better
 fn render(request : Request, template : &str, context : Option<HashMap<&str, &str>>) -> ~str {
-	// load the template file and run it through the parser
-	~"NOTHING HAHAHAHAHA"
+  // load the template file and run it through the parser
+  ~"NOTHING HAHAHAHAHA"
 }
 
 type Route<'a> = (&'a str, &'a str, fn(Request) -> Response);
@@ -66,16 +66,16 @@ type Route<'a> = (&'a str, &'a str, fn(Request) -> Response);
 //////////////////////////
 
 static routes: &'static [Route<'static>] = &[
-	("GET", "/", index),
-	("GET", "/test", index)
+  ("GET", "/", index),
+  ("GET", "/test", index)
 ];
 
 
 pub fn index(request: Request) -> Response {
-	return Response::new(status::Ok, render(request, "templates/index.html", None));
+  return Response::new(status::Ok, render(request, "templates/index.html", None));
 }
 
 fn main() {
-	//let server = Oxidize::new(8000);
-	//server.serve_forever();	
+  //let server = Oxidize::new(8000);
+  //server.serve_forever();
 }
