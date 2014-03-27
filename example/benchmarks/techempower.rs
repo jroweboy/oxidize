a8b29e0d03ff9ce82ac2bc7044a7165d6c9fa682
Clayton Watts <cletusw@gmail.com>
2014-03-27 12:43:46 -0600

    Initial implementation of /db

--- example/benchmarks/techempower.rs
+++ example/benchmarks/techempower.rs
@@ -7,19 +7,25 @@ extern crate extra;
 use oxidize::{Oxidize, Request, ResponseWriter, Config, MediaType};
 use oxidize::route::Router;
 use oxidize::route::regexrouter::{RegexRouter, RegexRoute, Route, Simple};
+use std::cast;
 use std::io::net::ip::SocketAddr;
+use std::rand::{task_rng, Rng};
 use serialize::{json, Encodable};
 
 use postgres::{PostgresConnection, PostgresStatement, NoSsl};
+use postgres::pool::PostgresConnectionPool;
 use postgres::types::ToSql;
 
 // Database
 static connectionString:    &'static str = "postgres://benchmarkdbuser:benchmarkdbpass@localhost/hello_world";
-static worldSelect:         &'static str = "SELECT id, randomNumber FROM World WHERE id = ?";
+static worldSelect:         &'static str = "SELECT id, randomNumber FROM World WHERE id = $1";
 static worldUpdate:         &'static str = "UPDATE World SET randomNumber = ? WHERE id = ?";
 static fortuneSelect:       &'static str = "SELECT id, message FROM Fortune;";
-static worldRowCount:       int = 10000;
+static worldRowCount:       i64 = 10000;
 static maxConnectionCount:  int = 256;
+static connectionPoolCount: uint = 5;
+
+static mut connectionPoolOption: Option<*()> = None;
 
 fn main() {
     let routes: ~[RegexRoute] = ~[
@@ -37,8 +43,12 @@ fn main() {
         bind_addr: from_str::<SocketAddr>("127.0.0.1:8001").unwrap(),
     };
 
-    //Database Connection Example
+    //Database Connection Pool
     let conn = PostgresConnection::connect(connectionString, &NoSsl);
+    let pool = PostgresConnectionPool::new(connectionString, NoSsl, connectionPoolCount);
+    unsafe {
+        connectionPoolOption = Some(cast::transmute(&pool));
+    }
 
     let server = Oxidize::new(conf, router as ~Router);
     server.serve();
@@ -46,8 +56,8 @@ fn main() {
 
 #[deriving(Encodable)]
 pub struct JSONWorld {
-    id: &'static int,
-    random_number: &'static int
+    id: i64,
+    random_number: i64
 }
 
 #[deriving(Encodable)]
@@ -77,7 +87,30 @@ fn json_handler(request: &Request, response: &mut ResponseWriter) {
 
 #[allow(unused_must_use)]
 fn single_query_handler(request: &Request, response: &mut ResponseWriter) {
+    unsafe {
+        match connectionPoolOption {
+            Some(connectionPoolVoidPointer) => {
+                let connectionPool: *PostgresConnectionPool = cast::transmute(connectionPoolVoidPointer);
+                let conn = (*connectionPool).get_connection();
+                let stmt = conn.prepare(worldSelect);
+                let mut rng = task_rng();
+                let row: i64 = rng.gen_range(1i64, worldRowCount + 1);
 
+                for row in stmt.query([&row as &ToSql]) {
+                    let world = JSONWorld {
+                        id: row[1],
+                        random_number: row[2],
+                    };
+                    
+                    response.write_content_auto(
+                        MediaType {type_: ~"application",subtype: ~"json",parameters: ~[]}, 
+                        json::Encoder::str_encode(&world)
+                    );
+                }
+            }
+            None => { println!("Could not get database connection pool");}
+        }
+    }
 }
 
 #[allow(unused_must_use)]
